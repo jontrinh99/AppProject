08b3698b3c0a51b5d1e8479c5cf8ef6673df4b7d
Joshua Bermudez <Josh130516@gmail.com>
2020-08-11 13:04:09 -0700

    Troubleshooting Database RESOLVED

--- TestApp1/AppDelegate.swift
+++ TestApp1/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import Firebase                                 // Firebase integrations
+import Firebase                                // Firebase integrations
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,8 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-       
-        FirebaseApp.configure()                 // Firebase integration
+        
+        FirebaseApp.configure()                  // Firebase integration
+        
         
         return true
         
